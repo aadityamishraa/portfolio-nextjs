f9bffafb162be184dbcabcb76d1dbfcf2abc0d28
aadityamishraa <aadityamishra216@gmail.com>
2024-08-25 03:01:44 +0530

    domain changed

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -21,13 +21,13 @@ export default function Contact() {
     <main className="max-w-7xl mx-auto md:px-16 px-6 lg:mt-32 mt-20">
       <PageHeading
         title="Contact"
-        description="I love collaborating with creative folks, so don't hesitate to reach out and say hi!"
+        description="I love collaborating with creative folks, so don't hesitate to reach out and say hi! 👋"
       />
       <figure className="my-6">
         <Slide delay={0.12} className="flex flex-wrap gap-2">
           <div className="w-full md:w-1/3 p-8 dark:bg-primary-bg bg-zinc-50 border border-transparent dark:hover:border-zinc-700 hover:border-zinc-200 rounded-lg">
             <form
-              action="https://formsubmit.com/db029a55bb2e015170b7ececeb43b58d"
+              action="https://formsubmit.co/db029a55bb2e015170b7ececeb43b58d"
               method="POST"
               className="flex flex-col gap-4"
               autoComplete="off"
